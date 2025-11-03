5a29ba3719f152c2f5db2e1daa8a5dad54488b44
Anamika Choudhary <anamika2004811@gmail.com>
2025-11-04 00:13:14 +0530

    fix 404 issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      < HashRouter>
         <div className="min-h-screen flex flex-col bg-soft-purple-50 text-charcoal">
           <Navbar />
           <main className="flex-grow">
@@ -41,7 +41,7 @@ const App = () => (
           </main>
           <Footer />
         </div>
-      </BrowserRouter>
+       </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
